9a5a7d884693321e024c99425d4371e45ebf8fa0
DamianBlum <62249397+DamianBlum@users.noreply.github.com>
2023-10-15 18:16:55 -0300

    Ya esta la tabla hecha

--- TP2.c
+++ TP2.c
@@ -3,101 +3,99 @@
  * reconoce el lenguaje de las expresiones regulares (suma del lenguaje de los enternosNumericos y de las operaciones*
  */
 #include <stdio.h>
-
+#include <string.h>
 int T(int q, int x);
 int conseguirIndice(int x);
 void vaciarString(char str[]);
+void conseguirToken(char token[20], int estado_actual);
+#define LENGTH 50
 
 int main(int argc, char **argv)
 {
     int x;
     int estado_actual = 0;
-    char string[100]; // Tamaño máximo de la cadena (ajusta según tus necesidades)
-    vaciarString(string);
+    char string[LENGTH]; // Tamaño máximo de la cadena (ajusta según tus necesidades)
+    memset(string,0,LENGTH);
     const char *nombreArchivo = "archivo.txt";
     FILE *archivo = fopen(nombreArchivo, "r");
-
+    char token[20];
     if (archivo == NULL)
     {
         printf("No se pudo abrir el archivo.\n");
         return 1;
     }
+    printf("+-------------+----------------------+\n");
+    printf("|En el codigo | Token                |\n");
+    printf("+-------------+----------------------+\n");
 
     int i = 0;
     x = fgetc(archivo);
 
     while (x != EOF)
     {
-         char caracter = (char)x;
-         printf("%c\n", caracter);
-        if (x == '\n' || x == '\r')
-        {
-            if (estado_actual == 1 || estado_actual == 3)
-            {
-                printf("ACEPTADA\n");
-                printf("%s", string);
-            }
-            else
-            {
-                printf("RECHAZADA\n");
-                printf("%s", string);
-            }
-            vaciarString(string);
+        if (x == '\n')
+        {   
+            conseguirToken(token, estado_actual+1);
+            printf("| %-11s | %-20.*s |\n", string, 20, token);
+            memset(string,0,LENGTH);
             estado_actual = 0;
             i = 0;
-            continue;
+            printf("+-------------+----------------------+\n");
+            printf("\n");
+            printf("+-------------+----------------------+\n");
+            printf("|En el codigo | Token                |\n");
+            printf("+-------------+----------------------+\n");
         }
         else
         {
             estado_actual = T(estado_actual, x);
             if (estado_actual == 4 || estado_actual == 2)
             {
-                printf("ACEPTADA: %s\n", string);
+                conseguirToken(token, estado_actual);
+                printf("| %-11s | %-20.*s |\n", string, 20, token);
+                //printf("ACEPTADA: %s\n", string);
                 ungetc(x, archivo);
-                vaciarString(string);
+                memset(string,0,LENGTH);
                 estado_actual = 0;
                 i = 0;
             }
+            
             else if (estado_actual == -1)
             {
                 printf("RECHAZADA\n");
                 printf("Caracter no válido: %c\n", (char)x); // Imprime el carácter no válido
                 estado_actual = 0;
+                memset(string,0,LENGTH);
                 i = 0;
             }
             else
             {
                 char caracter = (char)x;
-                // printf("%c\n", caracter);
                 string[i] = caracter;
-                /*for ( int j=0; j<6; j++) {
-                  printf("%c\n", string[j]);
-                }*/
+                // printf("%c\n", caracter);
+                //for ( int j=0; j<6; j++) {
+                //  printf("%c\n", string[j]);
+                //}
                 // printf("Cadena: %s\n", string);
                 i++;
             }
-            x = fgetc(archivo);
+            
         }
+        x = fgetc(archivo);
     }
+
     fclose(archivo); // Cierra el archivo
     if (estado_actual == 1 || estado_actual == 3)
     {
-        printf("ACEPTADA: %s\n", string);
+        conseguirToken(token, estado_actual+1);
+        printf("| %-11s | %-20.*s |\n", string, 20, token);
+        printf("+-------------+----------------------+\n");
     }
     else
     {
-        printf("RECHAZADO: %s\n", string);
+        printf("RECHAZADA: %s\n", string);
     }
     return 0;
-}
-
-void vaciarString(char str[])
-{
-
-    for (int i = 0; i < 100; i++)
-    {
-        str[i] = ' '; // Asignamos el valor nulo al primer elemento del arreglo
-    }
 }
 int conseguirIndice(int x)
 {
@@ -105,13 +103,24 @@ int conseguirIndice(int x)
     {
         return 0; // Dígitos del 0 al 9
     }
-    if (x >= 40 && x <= 43 || x == 45 || x == 47)
+    else if (x >= 40 && x <= 43 || x == 45 || x == 47)
     {
         return 1; // Caracteres operadores [(, ), +, *, - , /]
     }
-    return -1; // Caracteres no válidos
+    else {
+        return -1; // Caracteres no válidos
+    }
+
 }
 
+void conseguirToken(char token[20], int estado_actual) {
+    if (estado_actual == 2) {
+        strcpy(token, "Constante numerica");
+    } else {
+        strcpy(token, "Operador");
+    }
+    
+}
 int T(int q, int x)
 {
     int indice = conseguirIndice(x);
